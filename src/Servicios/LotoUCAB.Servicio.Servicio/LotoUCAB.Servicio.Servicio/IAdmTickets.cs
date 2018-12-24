eb7e2329ed7145f61d6cfd1ce8aa3431f4633f08
Gustavo Sanchez <gustavosr98@gmail.com>
2018-12-24 17:57:05 -0400

    Cambio de AdmSorteo a AdmTickets

--- src/Servicios/LotoUCAB.Servicio.Servicio/LotoUCAB.Servicio.Servicio/IAdmTickets.cs
+++ src/Servicios/LotoUCAB.Servicio.Servicio/LotoUCAB.Servicio.Servicio/IAdmTickets.cs
@@ -9,7 +9,7 @@ using System.Text;
 namespace LotoUCAB.Servicio.Servicio {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IAdmSorteos {
+    public interface IAdmTickets {
 
         [OperationContract]
         string GetData(int value);
