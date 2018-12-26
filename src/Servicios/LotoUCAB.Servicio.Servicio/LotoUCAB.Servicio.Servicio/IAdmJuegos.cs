2197ed3f3187bbfabc4b0c7a233dde4f758c42d1
Andrea Da Silva <andreadasilvab@gmail.com>
2018-12-25 18:14:42 -0600

    Prueba

--- src/Servicios/LotoUCAB.Servicio.Servicio/LotoUCAB.Servicio.Servicio/IAdmJuegos.cs
+++ src/Servicios/LotoUCAB.Servicio.Servicio/LotoUCAB.Servicio.Servicio/IAdmJuegos.cs
@@ -10,6 +10,7 @@ namespace LotoUCAB.Servicio.Servicio {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IAdmJuegos {
+        // Prueba
 
         [OperationContract]
         [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json)]
